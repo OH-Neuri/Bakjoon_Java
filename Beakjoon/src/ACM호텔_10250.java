4bc340879843f7fd9523d83985a4a7c7a2d1515d
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2022-08-02 23:56:50 +0900

    .

--- Beakjoon/src/ACM호텔_10250.java
+++ Beakjoon/src/ACM호텔_10250.java
@@ -4,37 +4,27 @@ import java.io.*;
 import java.util.*;
 
 public class ACM호텔_10250 {
-	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
 
-	public static void main(String[] args) throws IOException {
-		int count = 0;
-		int N = sc.nextInt();
-		for (int i = 0; i < N; i++) {
-			if (check() == true) {
-				count++;
-			}
-		}
-		System.out.println(count);
-	}
+		for (int i = 0; i < T; i++) {
+			int H = sc.nextInt();
+			int W = sc.nextInt();
+			int N = sc.nextInt();
+			int nCnt = 1;
+			int[][] acm = new int[W][H];
 
-	public static boolean check() {
-		boolean[] check = new boolean[26];
-		int prev = 0;
-		String string = sc.next();
-		for (int i = 0; i < string.length(); i++) {
-			int now = string.charAt(i);
-
-			if (prev != now) {
-				if (check[now - 'a'] == false) {
-					check[now - 'a'] = true;
-					prev = now;
-				} else {
-					return false;
+			end: for (int j = 0; j < W; j++) {
+				for (int k = 0; k < H; k++) {
+					if (N == nCnt) {
+						System.out.printf("%d%02d", k + 1, j + 1);
+						break end;
+					} else
+						nCnt++;
 				}
-			} else {
-				continue;
 			}
 		}
-		return true;
 	}
+
 }
--- Beakjoon/src/그룹단어체커_1316.java
+++ Beakjoon/src/그룹단어체커_1316.java
@@ -1,28 +1,39 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class 그룹단어체커_1316 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+	static Scanner sc = new Scanner(System.in);
 
-		for (int i = 0; i < T; i++) {
-			int H = sc.nextInt();
-			int W = sc.nextInt();
-			int N = sc.nextInt();
-			int nCnt = 1;
-			int[][] acm = new int[W][H];
-
-			end: for (int j = 0; j < W; j++) {
-				for (int k = 0; k < H; k++) {
-					if (N == nCnt) {
-						System.out.printf("%d%02d", k+1, j+1);
-						break end;
-					} else
-						nCnt++;
-				}
+	public static void main(String[] args) throws IOException {
+		int count = 0;
+		int N = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			if (check() == true) {
+				count++;
 			}
 		}
+		System.out.println(count);
 	}
-}
 
-//출력은 제대로 나오는데 틀렸습니다 뜸. 반례를 찾아봐야 할 것 같음
+	public static boolean check() {
+		boolean[] check = new boolean[26];
+		int prev = 0;
+		String string = sc.next();
+		for (int i = 0; i < string.length(); i++) {
+			int now = string.charAt(i);
+
+			if (prev != now) {
+				if (check[now - 'a'] == false) {
+					check[now - 'a'] = true;
+					prev = now;
+				} else {
+					return false;
+				}
+			} else {
+				continue;
+			}
+		}
+		return true;
+	}
+
+}
